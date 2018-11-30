2248297d398c951fd1ce9c7cd252c1ad33663756
emelendez <exmelendez@gmail.com>
2018-11-30 15:12:21 -0500

    mod/testing for spacing/collection

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var ldrField = "=LDR  00000cam\\\\2200000\\a\\4500";
+var zeroOneField = "=001  \\\\\\95022800";
 var entryArr = [];
 
 $(document).ready(function () {
@@ -6,17 +8,21 @@ $(document).ready(function () {
       
       var bookTitle = $('#book-title').val();
       var author = $('#author').val();
-      var userDetails = bookTitle + author;
-      entryArr.push(userDetails);
-      console.log(entryArr);
+    //   var titleDetails = bookTitle + author;
+      var titleDetails = ldrField + "\n" + zeroOneField + "\n\n";
+    
+
+      entryArr.push(titleDetails);
+    //   console.log(entryArr);
       document.getElementById("marc-form").reset();
     });
     
     $('#btnSaveNDownload').click(function () {
       var bookTitle = $('#book-title').val();
       var author = $('#author').val();
-      var userDetails = "=000  00000nam\\\\22000004a\\4500" + "\n" + bookTitle + "\n" + author;
-      
+      var userDetails = ldrField + "\n" + bookTitle + "\n" + author + "\n";
+      var test = "cool";
+ 
       var blob = new Blob([userDetails], 
                           {
                             type:"application/json;utf - 8"
@@ -26,5 +32,17 @@ $(document).ready(function () {
       userLink.setAttribute('download', bookTitle + '.txt');
       userLink.setAttribute('href', window.URL.createObjectURL(blob));
       userLink.click();
-     });
-  });
+    });
+
+    $('#btnSaveNDownload2').click(function () {
+        var blob = new Blob(entryArr, 
+                            {
+                              type:"application/json;utf - 8"
+                            }
+                           );
+        var userLink = document.createElement('a');
+        userLink.setAttribute('download', "someName" + '.txt');
+        userLink.setAttribute('href', window.URL.createObjectURL(blob));
+        userLink.click();
+      });
+});
