503ee878bf352e27f7ded9a173a0e79819a5a096
Eddie Melendez <exmelendez@gmail.com>
2018-12-02 09:55:03 -0500

    requirement changes + more tag field additions

--- js/main.js
+++ js/main.js
@@ -1,41 +1,44 @@
 var ldrTag = "=LDR  00000sam\\\\2200000\\a\\4500";
-var zeroOneField = "=001  \\\\\\95022800";
+var ctrlNumberTag = "=001  ";
+var ctrlNumIdTag = "=003  DLC";
+var dateTimeTranTag = "=005  ";
+var pubDataTag = "=008  ";
+var catalogAgencyTag = "=040  \\\\$aSBCSICA$cSBCSICA";
+var mainTitleTag = "=245  10$a";
+
 var entryArr = [];
 var entryNumber = 0;
 
-$(document).ready(function () { console.log(time());
+$(document).ready(function () {
   
     $('#new-entry').click(function(){
-      
-      var bookTitle = $('#book-title').val();
-      var author = $('#author').val();
-    //   var titleDetails = bookTitle + author;
-      var titleDetails = ldrField + "\n" + zeroOneField + "\n\n";
 
-      entryArr.push(titleDetails);
-      document.getElementById("marc-form").reset();
-      entryNumber++;
-      $("#entry-num").text("Entries: " + entryNumber);
-    });
-    
-    $('#btnSaveNDownload').click(function () {
-      var bookTitle = $('#book-title').val();
-      var author = $('#author').val();
-      var userDetails = ldrField + "\n" + bookTitle + "\n" + author + "\n";
-      var test = "cool";
- 
-      var blob = new Blob([userDetails], 
-                          {
-                            type:"application/json;utf - 8"
-                          }
-                         );
-      var userLink = document.createElement('a');
-      userLink.setAttribute('download', bookTitle + '.txt');
-      userLink.setAttribute('href', window.URL.createObjectURL(blob));
-      userLink.click();
+      if($('#pub-day').val().length === 1 || $('#pub-day').val() > 31 || $('#pub-year').val().length === 1 || $('#pub-year').val().length === 2 || $('#pub-year').val().length === 3) {
+        alert("incorrect value");
+      } else if($('#pub-month').val() === "" || $('#language').val() === "" || $('#book-title').val() === ""){
+        alert("missing required field");
+      } else {
+        var pubData = pubDataTagCreate($('#pub-year').val(), $('#pub-month').val(), $('#pub-day').val(), $('#language').val());
+        
+        var titleDetails = ldrTag + "\n" 
+        + ctrlNumberTag + marcDate("ctrlNum") + "\n" 
+        + ctrlNumIdTag + "\n" 
+        + dateTimeTranTag + marcDate("dateTimeTran") + "\n" 
+        + pubDataTag + pubData + "\n" 
+        + catalogAgencyTag + "\n"
+        + mainTitleTag + $('#book-title').val() + '.' +"\n\n";
+
+        entryArr.push(titleDetails);
+        document.getElementById("marc-form").reset();
+        entryNumber++;
+        $("#entry-num").text("Entries: " + entryNumber);
+      }
     });
 
     $('#btnSaveNDownload2').click(function () {
+      if(entryNumber === 0) {
+        alert("at least 1 entry must be entered");
+      } else {
         var blob = new Blob(entryArr, 
                             {
                               type:"application/json;utf - 8"
@@ -47,17 +50,35 @@ $(document).ready(function () { console.log(time());
         userLink.click();
         entryNumber = 0;
         $("#entry-num").text("Entries: " + entryNumber);
+        }
       });
 });
 
-function time() {
-  var ctrlNum = "";
+function pubDataTagCreate(year, month, day, lang) {
+  var twoDigitYear = "";
+  var fourDigitYear = year;
+
+  if(year === "") {
+    twoDigitYear = "\\\\";
+    fourDigitYear = "\\\\\\\\";
+  } else {
+    twoDigitYear = year.charAt(2) + year.charAt(3);
+    fourDigitYear = year;
+  }
+
+  if(day === "") {
+    day = "\\\\";
+  }
+
+  return twoDigitYear + month + day + 's' + fourDigitYear + "\\\\\\\\" + "xxu" + "|||||||||||||||||" + lang + "\\\\";
+}
+
+function marcDate(type) {
   var now = new Date();
 
   var month = now.getMonth();
   var day = now.getDate();
-  var yearInit = String(now.getFullYear());
-  var year = yearInit.charAt(2) + yearInit.charAt(3);
+  var fourDigYear = String(now.getFullYear());
   var hour = now.getHours();
   var minutes = now.getMinutes();
   var seconds = now.getSeconds();
@@ -78,12 +99,25 @@ function time() {
     seconds = '0'+ seconds;
   }
 
-  ctrlNum+= month;
-  ctrlNum+= day;
-  ctrlNum+= year;
-  ctrlNum+= hour;
-  ctrlNum+= minutes;
-  ctrlNum+= seconds;
+  switch (type) {
+    case "ctrlNum":
+      var year = fourDigYear.charAt(2) + fourDigYear.charAt(3);
+      var ctrlNum = "";
+      
+      ctrlNum+= month;
+      ctrlNum+= day;
+      ctrlNum+= year;
+      ctrlNum+= hour;
+      ctrlNum+= minutes;
+      ctrlNum+= seconds;
+    
+      return ctrlNum;
 
-  return ctrlNum;
+    case "dateTimeTran":
+      var dateTimeTranTag = "";
+      
+      dateTimeTranTag += fourDigYear + month + day + hour + minutes + seconds + ".0";
+
+      return dateTimeTranTag;
+  }
 }
