d20c3c6cf3f22d8f29b81b15dd8abde975550dea
Eddie Melendez <exmelendez@gmail.com>
2019-02-11 22:24:20 -0500

    only for testing

--- js/test2.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("wrapperID").onclick = function() {myFunction()};
-
-function myFunction() {
-    document.getElementById("wrapperID").style.display="none"; 
-	document.getElementById("testText").style.display="block"; 
-}
