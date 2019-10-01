8a5b711f13f3e74f4970f552ff13761d8c01c09d
Eddie Melendez <exmelendez@gmail.com>
2019-10-01 08:07:23 -0400

    form create process draft

--- js/form-manipulator.js
+++ js/form-manipulator.js
@@ -37,36 +37,119 @@ class FormCreator {
     
     */
 
-  createForm() {
-    const formTag = this.elementCreator("form");
-    const fieldsetTag = this.elementCreator("fieldset");
-    const labelTag = this.elementCreator("label");
-    const input = this.elementCreator("input");
-    const labelTag2 = this.elementCreator("label");
-    const input2 = this.elementCreator("input");
+  appendManualEntryForm() {
+    const FORM_TAG = this.elementCreator2({
+      type: "form",
+      value: null
+    });
 
-    input2.setAttribute("type", "text");
-    input2.setAttribute("name", "author-entry-inp");
-    input2.setAttribute("value", "");
+    const H1_TAG = this.elementCreator2({
+      type: "h1",
+      value: "Manual Entry"
+    });
 
-    labelTag2.setAttribute("class", "semi-req-field");
-    labelTag2.setAttribute("for", "author-ent-inp");
+    const FIELDSET_TAG = this.elementCreator2({
+      type: "fieldset",
+      value: null
+    });
 
-    labelTag.setAttribute("class", "req-field");
-    labelTag.setAttribute("for", "title-entry-inp");
+    const TITLE_LABEL = this.elementCreator2({
+      type: "label",
+      value: "Title:",
+      attributes: {
+        class: "req-field",
+        for: "title-entry-inp"
+      }
+    });
 
-    input.setAttribute("type", "text");
-    input.setAttribute("name", "title-entry-inp");
-    input.setAttribute("value", "");
+    const TITLE_INPUT = this.elementCreator2({
+      type: "input",
+      value: null,
+      attributes: {
+        type: "text",
+        name: "title-entry-inp"
+      }
+    });
 
-    labelTag.textContent = "Title:";
-    labelTag2.textContent = "Author:";
+    const AUTHOR_LABEL = this.elementCreator2({
+      type: "label",
+      value: "Author:",
+      attributes: {
+        class: "semi-req-field",
+        for: "author-entry-inp"
+      }
+    });
 
-    fieldsetTag.appendChild(labelTag);
-    fieldsetTag.appendChild(input);
-    fieldsetTag.appendChild(labelTag2);
-    fieldsetTag.appendChild(input2);
+    const AUTHOR_INPUT = this.elementCreator2({
+      type: "input",
+      value: null,
+      attributes: {
+        type: "text",
+        name: "title-entry-inp"
+      }
+    });
+
+    const LANGUAGE_LABEL = this.elementCreator2({
+      type: "label",
+      value: "Language:",
+      attributes: {
+        class: "req-field",
+        for: "language-entry-inp"
+      }
+    });
+
+    const LANGUAGE_SELECT = this.elementCreator2({
+      type: "select",
+      value: null,
+      attributes: {
+        name: "language-entry-inp"
+      }
+    });
+
+    const LANGUAGE_OPTION_DEFAULT = this.elementCreator2({
+      type: "option",
+      value: "Select Language",
+      attributes: {
+        selected: "",
+        disabled: ""
+      }
+    });
+
+    const LANGUAGE_OPTION_ENG = this.elementCreator2({
+      type: "option",
+      value: "English",
+      attributes: {
+        value: "eng"
+      }
+    });
+
+    const LANGUAGE_OPTION_SPA = this.elementCreator2({
+      type: "option",
+      value: "Spanish",
+      attributes: {
+        value: "spa"
+      }
+    });
+
+    const LANGUAGE_OPTION_DUAL = this.elementCreator2({
+      type: "option",
+      value: "Both",
+      attributes: {
+        value: "dual"
+      }
+    });
+
+    // Eventual appends
+
+    formTag.appendChild(h1Tag);
+
+    fieldsetTag.appendChild(titleLabel);
+    fieldsetTag.appendChild(titleInput);
+    fieldsetTag.appendChild(authorLabel);
+    fieldsetTag.appendChild(authorInput);
+
     formTag.appendChild(fieldsetTag);
+
     this.manInputDiv.appendChild(formTag);
   }
 
@@ -79,7 +162,30 @@ class FormCreator {
     return document.createElement(type);
   }
 
-  attributeAppender(element, type, value) {
-    element.setAttribute(type, value);
+  elementCreator2(elementObject) {
+    /*
+    const elementObject = {
+      type: "option",
+      value: "Select Language",
+      attributes: {
+        selected: "",
+        disabled: ""
+      }
+    };
+    */
+
+    let element = document.createElement(elementObject.type);
+
+    if (elementObject.value !== null) {
+      element.textContent = elementObject.value;
+    }
+
+    if (Object.keys(elementObject.attributes).length > 0) {
+      for (const key in elementObject.attributes) {
+        element.setAttribute(key, elementObject.attributes[key]);
+      }
+    }
+
+    return element;
   }
 }
